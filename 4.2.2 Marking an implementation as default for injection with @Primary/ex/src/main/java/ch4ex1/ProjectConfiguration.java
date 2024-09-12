ab9eecc07dc1f848bad9f5df041b98b786526224
Its-Maniaco <21123621+Its-Maniaco@users.noreply.github.com>
2024-09-13 02:29:56 +0530

    DI using Qualifier when muliple interface implementations

---
+++ 4.2.2 Marking an implementation as default for injection with @Primary/ex/src/main/java/ch4ex1/ProjectConfiguration.java
@@ -0,0 +1,10 @@
+package ch4ex1;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = { "proxies", "services", "repositories" })
+public class ProjectConfiguration {
+
+}
