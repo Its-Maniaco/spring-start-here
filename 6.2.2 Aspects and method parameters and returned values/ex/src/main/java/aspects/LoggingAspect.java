848b07336a6fe8b55c8f1b5bfc4ff79216a20218
Its-Maniaco <21123621+Its-Maniaco@users.noreply.github.com>
2024-09-18 01:07:04 +0530

    6.2.2 Aspects altering method parameters and returned values

--- 6.2.2 Aspects and method parameters and returned values/ex/src/main/java/aspects/LoggingAspect.java
+++ 6.2.2 Aspects and method parameters and returned values/ex/src/main/java/aspects/LoggingAspect.java
@@ -7,6 +7,8 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 
+import models.Comment;
+
 @Aspect
 public class LoggingAspect {
     private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
@@ -20,6 +22,18 @@ public class LoggingAspect {
 
         logger.info("Method: " + methodName);
         logger.info("Params: " + Arrays.asList(arguments));
+
+        // Uncomment below lines and comment the lines 38-41 to see:
+        // Aspects can change the parameters and also returned value
+
+        /*
+         * Comment comment = new Comment();
+         * comment.setText("Not original text");
+         * Object[] newArgs = { comment };
+         * Object returnedByMethod = joinPoint.proceed(newArgs);
+         * logger.info("NEW Method executed and returned " + returnedByMethod);
+         * return "FAILED";
+         */
 
         Object returnedByMethod = joinPoint.proceed();
         logger.info("Method executed and returned " + returnedByMethod);
