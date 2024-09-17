8bb8804d6b62d4c67bda4bfccdacfe44af1a5dad
Its-Maniaco <21123621+Its-Maniaco@users.noreply.github.com>
2024-09-17 18:48:32 +0530

    service before implementing aspect

---
+++ 6.2.1 Single Aspect/ex/src/main/java/services/CommentService.java
@@ -0,0 +1,16 @@
+package services;
+
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Service;
+
+import models.Comment;
+
+@Service
+public class CommentService {
+    private Logger logger = Logger.getLogger(CommentService.class.getName());
+
+    public void publishComment(Comment comment) {
+        logger.info("Publishing comment: " + comment.getText());
+    }
+}
